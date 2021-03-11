08f6eb81f89a8d1a21a4ee52512a69dc0c8f3034
Alexandra Rook <alexandra@washingtonabstract.com>
2021-03-10 17:13:30 -0700

    Adding postgres pipeline

--- city_scrapers/settings/prod.py
+++ city_scrapers/settings/prod.py
@@ -10,6 +10,8 @@ ITEM_PIPELINES = {
     "city_scrapers_core.pipelines.GCSDiffPipeline": 200,
     "city_scrapers_core.pipelines.MeetingPipeline": 300,
     "city_scrapers_core.pipelines.OpenCivicDataPipeline": 400,
+    # "city_scrapers.pipelines.TextExtractorPipeline": 500,
+    # "city_scrapers.pipelines.PostgresPipeline"
 }
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
@@ -28,7 +30,7 @@ FEED_EXPORTERS = {
 FEED_FORMAT = "jsonlines"
 
 FEED_STORAGES = {
-    "gcs": "scrapy.extensions.feedexport.GCSFeedStorage",
+    "gcs": "scrapy.extensions.feedexport.GCSFeedStorage".
 }
 
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
@@ -38,3 +40,7 @@ CITY_SCRAPERS_STATUS_BUCKET = GCS_BUCKET
 FEED_URI = (
     "gs://{bucket}/%(year)s/%(month)s/%(day)s/%(hour_min)s/%(name)s.json"
 ).format(bucket=GCS_BUCKET)
+
+POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
