cdd33950cba6c5a3c8482c04dd8f189efbab4021
Alexandra Rook <ali@abstract.us>
2021-10-21 14:00:38 -0600

    Added new link for la city government scraper

--- city_scrapers/spiders/la_city_government.py
+++ city_scrapers/spiders/la_city_government.py
@@ -13,18 +13,7 @@ class LaCityGovernmentSpider(CityScrapersSpider):
     agency = "Los Angeles City Government"
     timezone = "America/Los_Angeles"
     start_urls = [
-        (
-            "https://calendar.lacity.org/rest/views/calendar_rest_dynamic"
-            "?display_id=services_1"
-            "&display_id=services_1"
-            "&filters%5Beventtype%5D=686"
-            "&filters%5Bdepartment%5D="
-            "&filters%5Btags%5D="
-            "&filters%5Bstart%5D%5Bvalue%5D%5Bdate%5D="
-            f"{str(date.today()-timedelta(days=14))}"
-            "&filters%5Bend%5D%5Bvalue%5D%5Bdate%5D="
-            f"{str(date.today()+timedelta(days=14))}"
-        ),
+        f"https://lacity.primegov.com/api/v2/PublicPortal/ListArchivedMeetings?year={date.today().year}",
     ]
 
     def parse(self, response):
@@ -34,7 +23,6 @@ class LaCityGovernmentSpider(CityScrapersSpider):
         Change the `_parse_title`, `_parse_start`, etc methods to fit your scraping
         needs.
         """
-        self.logger.info("Parse function called on %s", response.url)
         json_response = response.json()
         for item in json_response:
             meeting = Meeting(
@@ -60,64 +48,51 @@ class LaCityGovernmentSpider(CityScrapersSpider):
     def _parse_title(self, item):
         """Parse or generate meeting title."""
         title = ""
-        if "rawtitle" in item and item["rawtitle"]:
-            title = unescape(item["rawtitle"])
-        elif "title" in item and item["title"]:
+        if "title" in item and item["title"]:
             title = unescape(item["title"])
         return title.strip()
 
     def _parse_description(self, item):
         """Parse or generate meeting description."""
         description = ""
-        if "descriptionraw" in item and item["descriptionraw"]:
-            description = unescape(item["descriptionraw"])
-        elif "description" in item and item["description"]:
-            description = unescape(item["description"])
+        if "allowPublicSpeaker" in item:
+            allowPublicSpeaker = "allows" if item["allowPublicSpeaker"] else "does not allow"
+            description += f"This meeting {allowPublicSpeaker} public speakers. "
+        if "allowPublicComment" in item:
+            allowPublicComment = "allows" if item["allowPublicComment"] else "does not allow"
+            description += f"This meeting {allowPublicComment} public speakers. "
+        if "isZoomMeeting" in item and item["isZoomMeeting"]:
+            description += f"This meeting is a Zoom meeting. "
+        if "meetingStatus" in item and item["meetingState"] == 1:
+            description += "This meeting has been cancelled. "
         return description.strip()
 
     def _parse_classification(self, item):
         """Parse or generate classification from allowed options."""
         for classification in CLASSIFICATIONS:
-            if classification in unescape(item["rawtitle"]):
+            if classification in unescape(item["title"]):
                 return classification
         return NOT_CLASSIFIED
 
     def _parse_start(self, item):
         """Parse start datetime as a naive datetime object."""
-        start = None
-        fields = ["start", "event-date-field-start", ["event-date", "event-time"]]
-        for field in fields:
-            start_str = ""
-            if type(field) is list:
-                for f in field:
-                    if f in item and item[f]:
-                        start_str += f"{item[f]} "
-            else:
-                if field in item and item[field]:
-                    start_str = item[field]
-            try:
-                start = parse(start_str)
-            except ParserError:
-                continue
-            return start.replace(tzinfo=None)
+        start_str = ""
+        if "date" in item:
+            start_str += item["date"] + " "
+        if "time" in item:
+            start_str += item["time"]
+        try:
+            start = parse(start_str)
+        except ParserError:
+            start = None
         return start.replace(tzinfo=None)
 
     def _parse_end(self, item):
         """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        end = None
-        if "end" in item and item["end"]:
-            try:
-                end = parse(item["end"]).replace(tzinfo=None)
-            except ParserError:
-                pass
-        if self._parse_start(item) == end:
-            return None
-        return end.replace(tzinfo=None)
+        return None
 
     def _parse_time_notes(self, item):
         """Parse any additional notes on the timing of the meeting"""
-        if "multiday" in item and item["multiday"] == "Yes":
-            return "This is a multiday event."
         return ""
 
     def _parse_all_day(self, item):
@@ -126,34 +101,30 @@ class LaCityGovernmentSpider(CityScrapersSpider):
 
     def _parse_location(self, item):
         """Parse or generate location."""
-        name = item["organization"]
-        address = ""
-        if "street" in item and item["street"]:
-            address += item["street"] + ", "
-        if "street2" in item and item["street2"]:
-            address += item["street2"] + ", "
-        if "city" in item and item["city"]:
-            address += item["city"] + ", "
-        if "state" in item and item["state"]:
-            address += item["state"] + ", "
-        if "country" in item and item["country"]:
-            address += item["country"] + " "
-        if "zipcode" in item and item["zipcode"]:
-            address += item["zipcode"]
         return {
-            "address": address.strip() if address else "",
-            "name": name.strip() if name else "",
+            "address": "",
+            "name": "",
         }
 
     def _parse_links(self, item):
         """Parse or generate links."""
         links = []
-        if "informationurl" in item and item["informationurl"]:
-            links.append(
-                {"href": item["informationurl"], "title": "Meeting/Agenda Information"}
-            )
-        if "url" in item and item["url"]:
-            links.append({"href": item["url"], "title": "Calendar Link"})
+        if "documentList" in item:
+            for doc in item["documentList"]:
+                if "compileOutputType" in doc and doc["compileOutputType"] == 1:
+                    link = "https://lacity.primegov.com/Public/CompiledDocument/" + doc["id"]
+                else:
+                    link = "https://lacity.primegov.com/Portal/Meeting?compiledMeetingDocumentFileId=" + doc["id"]
+                
+                if "templateName" in doc and doc["templateName"]:
+                    title = doc["tempmlateName"]
+                else:
+                    title = "Meeting/Agenda Information"
+                links.append(
+                    {"href": link, "title": title}
+                )
+        if "videoUrl" in item and item["videoUrl"]:
+            links.append({"href": item["videoUrl"], "title": "Video Link"})
         if len(links) == 0:
             links.append({"href": "", "title": ""})
         return links
