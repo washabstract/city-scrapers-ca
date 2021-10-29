3d4ef7a20ee0cfc7a5424e3694f33023d4cfaa83
Alexandra Rook <ali@abstract.us>
2021-10-29 11:42:06 -0600

    Fixing styling

--- city_scrapers/spiders/sf_planning.py
+++ city_scrapers/spiders/sf_planning.py
@@ -15,7 +15,7 @@ class SfPlanningSpider(CityScrapersSpider):
         "https://sfplanning.org/hearings-cpc-grid?keys=&"
         "field_events_timeframe_target_id=2&field_id_previous_years_mobile=All"
         "&field_id_upcoming_years_mobile=All&field_id_previous_years=All"
-        "&field_id_upcoming_years=All"
+        "&field_id_upcoming_years=All",
     ]
 
     def parse(self, response):
