eb8995b92e05f85b9833f8d72587a917d4a83d4b
Alexandra Rook <ali@abstract.us>
2022-02-03 20:15:34 -0700

    Import all core commands

---
+++ city_scrapers/commands/combinefeeds.py
@@ -0,0 +1,232 @@
+import json
+from datetime import datetime, timedelta
+from operator import itemgetter
+from urllib.parse import quote, urlparse
+
+from scrapy.commands import ScrapyCommand
+from scrapy.exceptions import UsageError
+
+
+class Command(ScrapyCommand):
+    requires_project = True
+
+    def syntax(self):
+        return "[options]"
+
+    def short_desc(self):
+        return "Combine all recent feeds into latest.json and upcoming.json"
+
+    def run(self, args, opts):
+        storages = self.settings.get("FEED_STORAGES", {})
+        if "s3" in storages:
+            self.combine_s3()
+        elif "azure" in storages:
+            self.combine_azure()
+        elif "gcs" in storages:
+            self.combine_gcs()
+        else:
+            raise UsageError(
+                "Either 's3', 'azure', or 'gcs' must be in FEED_STORAGES "
+                "to combine past feeds"
+            )
+
+    def combine_s3(self):
+        import boto3
+
+        parsed = urlparse(self.settings.get("FEED_URI"))
+        bucket = parsed.netloc
+        feed_prefix = self.settings.get("CITY_SCRAPERS_DIFF_FEED_PREFIX", "%Y/%m/%d")
+        client = boto3.client(
+            "s3",
+            aws_access_key_id=self.settings.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=self.settings.get("AWS_SECRET_ACCESS_KEY"),
+        )
+
+        max_days_previous = 3
+        days_previous = 0
+        prefix_objects = []
+        while days_previous <= max_days_previous:
+            prefix_objects = client.list_objects(
+                Bucket=bucket,
+                Prefix=(datetime.now() - timedelta(days=days_previous)).strftime(
+                    feed_prefix
+                ),
+            ).get("Contents", [])
+            if len(prefix_objects) > 0:
+                break
+            days_previous += 1
+
+        spider_keys = self.get_spider_paths([obj["Key"] for obj in prefix_objects])
+        meetings = []
+        for key in spider_keys:
+            feed_text = (
+                client.get_object(Bucket=bucket, Key=key)
+                .get("Body")
+                .read()
+                .decode("utf-8")
+            )
+            meetings.extend(
+                [json.loads(line) for line in feed_text.split("\n") if line.strip()]
+            )
+            # Copy latest results for each spider
+            spider_key = key.split("/")[-1]
+            client.copy_object(
+                Bucket=bucket,
+                Key=spider_key,
+                CopySource={"Bucket": bucket, "Key": key},
+            )
+        meetings = sorted(meetings, key=itemgetter(self.start_key))
+        yesterday_iso = (datetime.now() - timedelta(days=1)).isoformat()[:19]
+        upcoming = [
+            meeting
+            for meeting in meetings
+            if meeting[self.start_key][:19] > yesterday_iso
+        ]
+
+        client.put_object(
+            Body=("\n".join([json.dumps(meeting) for meeting in meetings])).encode(),
+            Bucket=bucket,
+            CacheControl="no-cache",
+            Key="latest.json",
+        )
+        client.put_object(
+            Body=("\n".join([json.dumps(meeting) for meeting in upcoming])).encode(),
+            Bucket=bucket,
+            CacheControl="no-cache",
+            Key="upcoming.json",
+        )
+
+    def combine_azure(self):
+        from azure.storage.blob import ContainerClient, ContentSettings
+
+        feed_uri = self.settings.get("FEED_URI")
+        feed_prefix = self.settings.get("CITY_SCRAPERS_DIFF_FEED_PREFIX", "%Y/%m/%d")
+        account_name, account_key = feed_uri[8::].split("@")[0].split(":")
+        container = feed_uri.split("@")[1].split("/")[0]
+        container_client = ContainerClient(
+            f"{account_name}.blob.core.windows.net",
+            container,
+            credential=account_key,
+        )
+
+        max_days_previous = 3
+        days_previous = 0
+        prefix_blobs = []
+        while days_previous <= max_days_previous:
+            prefix_blobs = [
+                blob
+                for blob in container_client.list_blobs(
+                    name_starts_with=(
+                        datetime.now() - timedelta(days=days_previous)
+                    ).strftime(feed_prefix)
+                )
+            ]
+            if len(prefix_blobs) > 0:
+                break
+            days_previous += 1
+
+        spider_blob_names = self.get_spider_paths([blob.name for blob in prefix_blobs])
+        meetings = []
+        for blob_name in spider_blob_names:
+            feed_blob = container_client.get_blob_client(blob_name)
+            feed_text = feed_blob.download_blob().content_as_text()
+            meetings.extend(
+                [json.loads(line) for line in feed_text.split("\n") if line]
+            )
+            # Copy latest results for each spider
+            spider_blob_name = blob_name.split("/")[-1]
+            spider_blob = container_client.get_blob_client(spider_blob_name)
+            spider_blob.start_copy_from_url(
+                f"https://{account_name}.blob.core.windows.net"
+                f"/{quote(container)}/{blob_name}"
+            )
+        meetings = sorted(meetings, key=itemgetter(self.start_key))
+        yesterday_iso = (datetime.now() - timedelta(days=1)).isoformat()[:19]
+        upcoming = [
+            meeting
+            for meeting in meetings
+            if meeting[self.start_key][:19] > yesterday_iso
+        ]
+
+        container_client.upload_blob(
+            "latest.json",
+            "\n".join([json.dumps(meeting) for meeting in meetings]),
+            content_settings=ContentSettings(cache_control="no-cache"),
+            overwrite=True,
+        )
+
+        container_client.upload_blob(
+            "upcoming.json",
+            "\n".join([json.dumps(meeting) for meeting in upcoming]),
+            content_settings=ContentSettings(cache_control="no-cache"),
+            overwrite=True,
+        )
+
+    def combine_gcs(self):
+        from google.cloud import storage
+
+        parsed = urlparse(self.settings.get("FEED_URI"))
+        bucket_name = parsed.netloc
+        feed_prefix = self.settings.get("CITY_SCRAPERS_DIFF_FEED_PREFIX", "%Y/%m/%d")
+        client = storage.Client()
+        bucket = client.bucket(bucket_name)
+
+        max_days_previous = 3
+        days_previous = 0
+        prefix_blobs = []
+        while days_previous <= max_days_previous:
+            prefix_blobs = client.list_blobs(
+                bucket,
+                prefix=(datetime.now() - timedelta(days=days_previous)).strftime(
+                    feed_prefix
+                ),
+            )
+            prefix_blobs = [blob for blob in prefix_blobs]
+            if len(prefix_blobs) > 0:
+                break
+            days_previous += 1
+
+        meetings = []
+        for blob in prefix_blobs:
+            feed_text = blob.download_as_bytes().decode("utf-8")
+            meetings.extend(
+                [json.loads(line) for line in feed_text.split("\n") if line.strip()]
+            )
+            # Copy latest results for each spider
+            spider_name = blob.name.split("/")[-1]
+            bucket.copy_blob(blob, bucket, new_name=spider_name)
+        meetings = sorted(meetings, key=itemgetter(self.start_key))
+        yesterday_iso = (datetime.now() - timedelta(days=1)).isoformat()[:19]
+        upcoming = [
+            meeting
+            for meeting in meetings
+            if meeting[self.start_key][:19] > yesterday_iso
+        ]
+
+        new_meetings_blob = bucket.blob("latest.json")
+        new_meetings_blob.upload_from_string(
+            "\n".join([json.dumps(meeting) for meeting in meetings]).encode()
+        )
+        new_upcoming_blob = bucket.blob("upcoming.json")
+        new_upcoming_blob.upload_from_string(
+            "\n".join([json.dumps(meeting) for meeting in upcoming]).encode()
+        )
+
+    def get_spider_paths(self, path_list):
+        """Get a list of the most recent scraper results for each spider"""
+        spider_paths = []
+        for spider in self.crawler_process.spider_loader.list():
+            all_spider_paths = [p for p in path_list if f"{spider}." in p]
+            if len(all_spider_paths) > 0:
+                spider_paths.append(sorted(all_spider_paths)[-1])
+        return spider_paths
+
+    @property
+    def start_key(self):
+        pipelines = self.settings.get("ITEM_PIPELINES", {})
+        if (
+            "city_scrapers_core.pipelines.OpenCivicDataPipeline" in pipelines
+            or "city_scrapers.pipelines.OpenCivicDataPipeline" in pipelines
+        ):
+            return "start_time"
+        return "start"
---
+++ city_scrapers/commands/validate.py
@@ -0,0 +1,59 @@
+import logging
+import os
+from importlib import import_module
+
+from city_scrapers_core.pipelines import ValidationPipeline
+from scrapy.commands import ScrapyCommand
+
+logger = logging.getLogger(__name__)
+
+
+class Command(ScrapyCommand):
+    requires_project = True
+
+    def syntax(self):
+        return "[options] <spider>"
+
+    def short_desc(self):
+        return "Run a spider with validations, or validate all changed spiders in a PR"
+
+    def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option(
+            "--all",
+            dest="all",
+            action="store_true",
+            help="Run validation on all scrapers",
+        )
+
+    def run(self, args, opts):
+        self._add_validation_pipeline()
+        spider_list = self.crawler_process.spider_loader.list()
+        spiders = [spider for spider in args if spider in spider_list]
+        if len(spiders) == 0 and not opts.all:
+            logger.info("No spiders provided, exiting...")
+            return
+        elif opts.all:
+            spiders = spider_list
+        for spider in spiders:
+            self.crawler_process.crawl(spider)
+        self.crawler_process.start()
+
+    def _add_validation_pipeline(self):
+        """Add validation pipeline to pipelines if not already present"""
+        pipelines = self.settings.get("ITEM_PIPELINES", {})
+        pipeline_name = ValidationPipeline.__name__
+        # Exit if pipeline already included
+        if any(pipeline_name in pipeline for pipeline in pipelines.keys()):
+            return
+        fullname = f"{ValidationPipeline.__module__}.{pipeline_name}"
+        priority = 1
+        if len(pipelines.keys()) > 0:
+            priority = max(pipelines.values()) + 1
+        self.settings.set("ITEM_PIPELINES", {**pipelines, **{fullname: priority}})
+        self.settings.set("CITY_SCRAPERS_ENFORCE_VALIDATION", True)
+
+    @property
+    def spiders_dir(self):
+        spiders_module = import_module(self.settings.get("NEWSPIDER_MODULE"))
+        return os.path.relpath(os.path.dirname(spiders_module.__file__))
