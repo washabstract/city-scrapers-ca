ff3cf8e4a65fd498badb451c2d38e47708fa28f5
Alexandra Rook <ali@abstract.us>
2022-02-03 19:27:49 -0700

    isort .

--- city_scrapers/pipelines/__init__.py
+++ city_scrapers/pipelines/__init__.py
@@ -1,7 +1,7 @@
+from .diff import AzureDiffPipeline, GCSDiffPipeline, S3DiffPipeline
+from .ocd import OpenCivicDataPipeline
 from .postgres import PostgresPipeline
 from .text_extractor import TextExtractorPipeline
-from .ocd import OpenCivicDataPipeline
-from .diff import AzureDiffPipeline, GCSDiffPipeline, S3DiffPipeline
 
 __all__ = [
     "AzureDiffPipeline",
