6be2211814a30e05e6a56524edea5331ead4e037
panayiotishalios <panayiotis.halios@gmail.com>
2022-12-07 16:03:57 -0800

    Tested lomita

---
+++ city_scrapers/spiders/lomita.py
@@ -0,0 +1,23 @@
+from city_scrapers.spiders.granicus import GranicusSpider
+
+
+class LomitaSpider(GranicusSpider):
+    name = "lomita"
+    agency = "Lomita"
+    sub_agency = "Council"
+    start_urls = ["https://lomita.granicus.com/ViewPublisher.php?view_id=3"]
+    location = {
+        "name": "",
+        "address": "24300 NARBONNE AVENUE, LOMITA, CA 90717",
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            self.name,
+            self.agency,
+            self.location,
+            self.sub_agency,
+            None,
+            *args,
+            **kwargs,
+        )
