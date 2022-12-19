255b1a59799177784576d2dac9749463f0f6d143
panayiotishalios <panayiotis.halios@gmail.com>
2022-12-19 16:39:59 +0200

    tested la_canada_flintridge

---
+++ city_scrapers/spiders/la_canada_flintridge.py
@@ -0,0 +1,25 @@
+from city_scrapers.spiders.granicus import GranicusSpider
+
+
+class LaCanadaFlintridgeSpider(GranicusSpider):
+    name = "la_canada_flintridge"
+    agency = "LA Canada Flintridge"
+    sub_agency = "Planning Commission"
+    start_urls = [
+        "https://lacanadaflintridge-ca.granicus.com" "/ViewPublisher.php?view_id=4"
+    ]
+    location = {
+        "name": "City Hall Council Chambers",
+        "address": "One Civic Center Drive, La Cañada Flintridge, CA 91011",
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            self.name,
+            self.agency,
+            self.sub_agency,
+            self.location,
+            None,
+            *args,
+            **kwargs,
+        )
