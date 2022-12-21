fb4ec82e0bc2bf94d1f2f0fecf28f2e02b2da9d5
panayiotishalios <panayiotis.halios@gmail.com>
2022-12-21 18:16:07 +0200

    Tested la habra heights

---
+++ city_scrapers/spiders/la_habra_heights.py
@@ -0,0 +1,25 @@
+from city_scrapers.spiders.granicus import GranicusSpider
+
+
+class LaHabraHeightsSpider(GranicusSpider):
+    name = "la_habra_heights"
+    agency = "La Habra Heights"
+    sub_agency = "Planning Commission"
+    timezone = "America/Chicago"
+    start_urls = ["https://la-habra-heights.granicus.com/ViewPublisher.php?view_id=4"]
+
+    location = {
+        "name": "",
+        "address": "1225 North Hacienda Road, La Habra Heights, California 90631",
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            self.name,
+            self.agency,
+            self.sub_agency,
+            self.location,
+            None,
+            *args,
+            **kwargs,
+        )
