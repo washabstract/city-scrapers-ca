93f6e4b4f639ac3e683f604db824007b36c0d542
panayiotishalios <panayiotis.halios@gmail.com>
2023-01-10 14:35:40 -0800

    Hermosa beach implemented

---
+++ city_scrapers/spiders/hermosa_beach.py
@@ -0,0 +1,23 @@
+from city_scrapers.spiders.granicus import GranicusSpider
+
+
+class HermosaBeachSpider(GranicusSpider):
+    name = "hermosa_beach"
+    agency = "Los Angeles"
+    sub_agency = "Hermosa Beach"
+    start_urls = ["https://hermosabeach.granicus.com/ViewPublisher.php?view_id=6"]
+    location = {
+        "name": "City Hall",
+        "address": "1315 Valley Drive Hermosa Beach, CA 90254",
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            self.name,
+            self.agency,
+            self.sub_agency,
+            self.location,
+            None,
+            *args,
+            **kwargs,
+        )
