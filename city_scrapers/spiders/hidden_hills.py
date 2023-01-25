611583b61fcdd6db0200b2fe3b40fcb063c66d17
panayiotishalios <panayiotis.halios@gmail.com>
2023-01-24 16:38:35 -0800

    Changed the agency and sub agency

--- city_scrapers/spiders/hidden_hills.py
+++ city_scrapers/spiders/hidden_hills.py
@@ -3,8 +3,8 @@ from city_scrapers.spiders.granicus import GranicusSpider
 
 class HiddenHillsSpider(GranicusSpider):
     name = "hidden_hills"
-    agency = "Los Angeles"
-    sub_agency = "Hidden Hills"
+    agency = "Hidden Hills"
+    sub_agency = "City"
     start_urls = ["https://hiddenhillscity.granicus.com/ViewPublisher.php?view_id=1"]
     location = {"name": "", "address": "6165 Spring Valley Rd, Hidden Hills, CA 91302"}
 
