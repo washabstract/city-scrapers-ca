a4fe4bb83aa84e42dbc451ff778c6ab5d22db834
panayiotishalios <panayiotis.halios@gmail.com>
2023-02-25 19:52:59 -0800

    Using a loop to go through each of the scrapers.

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 echo "Running scrapers"
 pipenv run scrapy list
-pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} -v
+
+# Assign variable to list of scrapers
+SCRAPERS=$(pipenv run scrapy list)
+
+# Loop through each scraper
+for scraper in $SCRAPERS
+do
+    echo "Running $scraper"
+    pipenv run scrapy crawl $scraper
+done
