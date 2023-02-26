65a660e9ad5e0278c3b0e6127e18c0a386cabb55
panayiotishalios <panayiotis.halios@gmail.com>
2023-02-25 20:08:53 -0800

    printing the scrapers that failed

--- .deploy.sh
+++ .deploy.sh
@@ -5,9 +5,20 @@ pipenv run scrapy list
 # Assign variable to list of scrapers
 SCRAPERS=$(pipenv run scrapy list)
 echo $SCRAPERS
+
+# create a list of scrapers that failed
+FAILED_SCRAPERS=()
+
 # Loop through each scraper
 for scraper in $SCRAPERS
 do
     echo "Running $scraper"
-    pipenv run scrapy crawl $scraper
+    pipenv run scrapy crawl $scraper -v
+
+    # check if the previous command failed and add the scraper to the list of failed scrapers
+    if [ $? -ne 0 ]; then
+        FAILED_SCRAPERS+=($scraper)
+    fi
 done
+
+echo "Scrapers that failed: ${FAILED_SCRAPERS[@]}"
