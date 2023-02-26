cf2cc134256459e7f6b998a886de1bb94fcf6c0b
panayiotishalios <panayiotis.halios@gmail.com>
2023-02-25 20:21:43 -0800

    Doesn't like the text after?

--- .deploy.sh
+++ .deploy.sh
@@ -16,9 +16,9 @@ do
     pipenv run scrapy crawl -v $scraper
 
     # check if the previous command failed and add the scraper to the list of failed scrapers
-    if [ $? -ne 0 ]; then
-        FAILED_SCRAPERS+=($scraper)
-    fi
+    # if [ $? -ne 0 ]; then
+    #     FAILED_SCRAPERS+=($scraper)
+    # fi
 done
 
 echo "Scrapers that failed: ${FAILED_SCRAPERS[@]}"
