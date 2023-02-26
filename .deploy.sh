471c49d1665d0024a1731d64c89757b6b31378a2
panayiotishalios <panayiotis.halios@gmail.com>
2023-02-25 19:32:43 -0800

    Running the scrapers with logs enabled to prevent blocking.

--- .deploy.sh
+++ .deploy.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} -s LOG_ENABLED=False &
-
-# Output to the screen every 9 minutes to prevent a travis timeout
-# https://stackoverflow.com/a/40800348
-export PID=$!
-while [[ `ps -p $PID | tail -n +2` ]]; do
-  echo 'Deploying'
-  sleep 30
-done
+pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {}
