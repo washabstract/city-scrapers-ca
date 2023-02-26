332d4a811b9bc4f0855a16d74d77f691dfb3a97e
panayiotishalios <panayiotis.halios@gmail.com>
2023-02-25 19:48:37 -0800

    enabling verbose

--- .deploy.sh
+++ .deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running scrapers"
 pipenv run scrapy list
-pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {}
+pipenv run scrapy list | xargs -I {} pipenv run scrapy crawl {} -v
